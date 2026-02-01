ed806e88ed2ab96df36f7c3dbe33e88140d17cb0
Chris Sterritt <csterritt@gmail.com>
2026-02-01 14:25:12 -0500

    Further setup for styling, routing.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script setup lang="ts"></script>
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
+  <nav class="flex gap-2">
+    <RouterLink to="/" class="btn btn-primary">Go to Home</RouterLink>
+    <RouterLink to="/about" class="btn btn-primary">Go to About</RouterLink>
+  </nav>
+  <RouterView />
 </template>
-
-<style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeView from '@/components/HomeView.vue'
+import AboutView from '@/components/AboutView.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
-
 export default router
